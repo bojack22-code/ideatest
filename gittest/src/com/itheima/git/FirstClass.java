aa810ec344962e2607926378dd48c4ee3919f8f1
bojack22-code <66618141+bojack22-code@users.noreply.github.com>
2020-06-08 20:51:41 +0800

    Update FirstClass.java
    add some word

--- gittest/src/com/itheima/git/FirstClass.java
+++ gittest/src/com/itheima/git/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
     }
-    public void test(){
+    public void test(){  #new test
         int i = 0;
         i+=10;
     }
